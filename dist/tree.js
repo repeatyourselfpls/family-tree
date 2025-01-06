67a2a76a2971d3dd6e2795209671c98ae8dbf4a3
repeatyourselfpls <surya.poddutoori@gmail.com>
2025-01-05 23:48:49 -0500

    tree alg with contours fixd

--- dist/tree.js
+++ dist/tree.js
@@ -33,34 +33,27 @@ class TreeNode {
         }
         else if (node.children.length == 1) {
             if (!node.previousSibling) {
-                node.X = node.getLeftMostNode().X;
+                node.X = node.getLeftMostChildNode().X;
             }
             else {
                 node.X = node.previousSibling.X + TreeNode.NODE_SIZE + TreeNode.SIBLING_DISTANCE;
-                node.mod = node.X - node.getLeftMostNode().X;
+                node.mod = node.X - node.getLeftMostChildNode().X;
             }
         }
         else {
             if (!node.previousSibling) {
-                node.X = (node.getLeftMostNode().X + node.getRightMostNode().X) / 2;
+                node.X = (node.getLeftMostChildNode().X + node.getRightMostChildNode().X) / 2;
             }
             else {
                 node.X = node.previousSibling.X + TreeNode.NODE_SIZE + TreeNode.SIBLING_DISTANCE;
-                node.mod = node.X - (node.getLeftMostNode().X + node.getRightMostNode().X) / 2; // currentX - desired
+                node.mod = node.X - (node.getLeftMostChildNode().X + node.getRightMostChildNode().X) / 2; // currentX - desired
             }
         }
         // console.log(`Calculating ${node.name}: (${node.X}, ${node.Y}) Mod: ${node.mod}`)
         // check if subtrees clash, adjust X and mod if so
-        if (node.children.length > 1) {
-            this.checkConflicts(node);
-        }
-    }
-    finalizeX(node, modSum) {
-        node.X += modSum;
-        modSum += node.mod;
-        for (const child of node.children) {
-            this.finalizeX(child, modSum);
-        }
+        // if (node.children.length > 1) {
+        //   this.checkConflicts(node)
+        // }
     }
     static checkConflicts(node) {
         // get left contour of right subtree and right contour of left subtree
@@ -68,7 +61,7 @@ class TreeNode {
             // if (node.children[i-1].children.length && node.children[i].children.length) {
             const rightNode = node.children[i];
             const leftNode = node.children[i - 1];
-            const rightContour = TreeNode.getRightContour(leftNode);
+            let rightContour = TreeNode.getRightContour(leftNode);
             const leftContour = TreeNode.getLeftContour(rightNode);
             // you only need to check minimum depth
             const depth = Math.min(rightContour.length, leftContour.length);
@@ -77,11 +70,13 @@ class TreeNode {
                 if (distance < 0) {
                     rightNode.X += Math.abs(distance) + TreeNode.SIBLING_DISTANCE + TreeNode.NODE_SIZE;
                     rightNode.mod += Math.abs(distance) + TreeNode.SIBLING_DISTANCE + TreeNode.NODE_SIZE;
+                    // TODO: Recalculate contours afterwards?
+                    rightContour = TreeNode.getRightContour(rightNode);
                 }
             }
         }
     }
-    // for each level, returns the list of leftmost node
+    // level order traversal of leftmost node's X positions w/ modifier added
     static getLeftContour(node) {
         const contour = [];
         const queue = [[node, 0]];
@@ -98,7 +93,7 @@ class TreeNode {
         }
         return contour;
     }
-    // for each level, returns the list of rightmost nodes
+    // level order traversal of rightmost node's X positions w/ modifier added
     static getRightContour(node) {
         const contour = [];
         const queue = [[node, 0]];
@@ -115,25 +110,60 @@ class TreeNode {
         }
         return contour;
     }
+    static finalizeX(node, modSum) {
+        node.X += modSum;
+        modSum += node.mod;
+        for (const child of node.children) {
+            this.finalizeX(child, modSum);
+        }
+    }
+    static levelOrderTraversal(node) {
+        const queue = [[node, 0]];
+        const traversal = [];
+        while (queue.length) {
+            const [curr, level] = queue.splice(0, 1)[0];
+            traversal.push([curr, level]);
+            if (curr.children.length) {
+                for (const child of curr.children) {
+                    queue.push([child, level + 1]);
+                }
+            }
+        }
+        return traversal;
+    }
     isLeafNode() {
         return this.children.length == 0;
     }
-    getRightMostNode() {
+    getRightMostChildNode() {
         return this.children.slice(-1)[0];
     }
-    getLeftMostNode() {
+    getLeftMostChildNode() {
         return this.children[0];
     }
 }
 TreeNode.NODE_SIZE = 1;
 TreeNode.SIBLING_DISTANCE = 0;
-const sunny = new TreeNode("Sunny", []);
-const vivian = new TreeNode("Vivian", []);
-const raj = new TreeNode("Raj", []);
-const sam = new TreeNode("Sam", []);
-const alice = new TreeNode("Alice", [sunny, vivian]);
-const bob = new TreeNode("Bob", [raj, sam, alice]);
-TreeNode.initializeNodes(bob, null, 0);
-TreeNode.calculateXMod(bob);
-console.log(TreeNode.getLeftContour(bob));
-console.log(TreeNode.getRightContour(bob));
+const B = new TreeNode("B", []);
+const C = new TreeNode("C", []);
+const H = new TreeNode("H", []);
+const I = new TreeNode("I", []);
+const J = new TreeNode("J", []);
+const K = new TreeNode("K", []);
+const L = new TreeNode("L", []);
+const A = new TreeNode("A", []);
+const D = new TreeNode("D", [B, C]);
+const G = new TreeNode("G", []);
+const M = new TreeNode("M", [H, I, J, K, L]);
+const E = new TreeNode("E", [A, D]);
+const F = new TreeNode("F", []);
+const N = new TreeNode("N", [G, M]);
+const O = new TreeNode("O", [E, F, N]);
+TreeNode.initializeNodes(O, null, 0);
+TreeNode.calculateXMod(O);
+TreeNode.finalizeX(O, 0);
+const traversedNodes = TreeNode.levelOrderTraversal(O);
+for (const [n, level] of traversedNodes) {
+    console.log(level, n.name, n.X);
+}
+console.log(TreeNode.getLeftContour(O));
+console.log(TreeNode.getRightContour(O));
