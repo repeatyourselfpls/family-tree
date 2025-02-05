62bc4fd10ffc9468ad6e1e826f79856acc5086e6
repeatyourselfpls <surya.poddutoori@gmail.com>
2025-02-05 02:42:40 -0500

    Added back dist lolo

---
+++ dist/draw.js
@@ -0,0 +1,75 @@
+const RADIUS = 50;
+const SPACING_X = RADIUS * 2 * 1.25;
+const SPACING_Y = 150;
+export function setupBackground(ctx, width, height) {
+    ctx.fillStyle = "wheat";
+    ctx.fillRect(0, 0, width, height);
+    ctx.strokeStyle = "rgba(0 0 0 / 5%)";
+    ctx.lineWidth = 1;
+    for (let i = 0; i < width; i += 15) {
+        ctx.beginPath();
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, height);
+        ctx.stroke();
+    }
+    for (let i = 0; i < height; i += 15) {
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(width, i);
+        ctx.stroke();
+    }
+}
+export function fitToScreen(leveledNodes, width, height) {
+    const startX = width / 2 - (leveledNodes[0][0].X * SPACING_X);
+    const startY = 100;
+    for (const [n, _] of leveledNodes) {
+        n.X = startX + (n.X * SPACING_X);
+        n.Y = startY + (n.Y * SPACING_Y);
+    }
+    return leveledNodes.map(([n, _]) => n);
+}
+// pre order traversal drawing
+export function drawAllNodes(ctx, node, parentNode) {
+    drawNode(ctx, node.X, node.Y, node.name);
+    if (parentNode) {
+        drawParentLink(ctx, node, parentNode);
+    }
+    for (const child of node.children) {
+        drawAllNodes(ctx, child, node);
+    }
+}
+function drawNode(ctx, x, y, val) {
+    ctx.fillStyle = "pink";
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(x, y, 50, degreeToRadians(0), degreeToRadians(360), false);
+    ctx.fill();
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.font = "24px arial";
+    const textWidth = ctx.measureText(`${val}`).width;
+    const textHeight = ctx.measureText("M").width;
+    ctx.fillText(`${val}`, x - textWidth / 2, y + textHeight / 2);
+}
+function drawParentLink(ctx, node, parent) {
+    const parent_x = parent.X;
+    const parent_y = parent.Y;
+    const node_x = node.X;
+    const node_y = node.Y;
+    const deltaX = node_x - parent_x;
+    const deltaY = node_y - parent_y;
+    const theta = Math.atan(deltaY / Math.abs(deltaX)); // positive val
+    const parent_x_delta = deltaX < 0 ? -1 * Math.cos(theta) * RADIUS : Math.cos(theta) * RADIUS;
+    const parent_y_delta = Math.sin(theta) * RADIUS;
+    const node_x_delta = -1 * parent_x_delta;
+    const node_y_delta = -1 * parent_y_delta;
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(parent_x + parent_x_delta, parent_y + parent_y_delta);
+    ctx.lineTo(node_x + node_x_delta, node_y + node_y_delta);
+    ctx.stroke();
+}
+function degreeToRadians(deg) {
+    return (deg / 360) * (2 * Math.PI);
+}
